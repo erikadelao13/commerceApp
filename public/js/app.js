551ab1360a873ccbb6eef15d278d4bda6ed96e21
erikadelao13 <00064815@uca.edu.sv>
2019-07-08 17:56:32 -0600

    second commit

--- public/js/app.js
+++ public/js/app.js
@@ -116,8 +116,8 @@ var gretting = 'hello';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/cesaramirez/Code/Elaniin/Internal/Bitlab/nano-studio/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/cesaramirez/Code/Elaniin/Internal/Bitlab/nano-studio/resources/scss/app.scss */"./resources/scss/app.scss");
+__webpack_require__(/*! C:\Users\Erika}\Desktop\Erika\BitlabProjects\boilerplate-tailwindcss-master\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Erika}\Desktop\Erika\BitlabProjects\boilerplate-tailwindcss-master\resources\scss\app.scss */"./resources/scss/app.scss");
 
 
 /***/ })
